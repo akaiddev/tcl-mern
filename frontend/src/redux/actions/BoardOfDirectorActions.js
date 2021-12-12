24135fb6d04f7ff8be348f34b938eb6c93aeb34f
abdurrahmanakaid <akaid.dev@gmail.com>
2021-12-12 11:25:01 -0800

    Redux add board Of Director

---
+++ frontend/src/redux/actions/BoardOfDirectorActions.js
@@ -0,0 +1,100 @@
+import axios from 'axios'
+import {
+  BOARD_OF_DIRECTOR_CREATE_FAIL,
+  BOARD_OF_DIRECTOR_CREATE_REQUEST,
+  BOARD_OF_DIRECTOR_CREATE_SUCCESS,
+  BOARD_OF_DIRECTOR_DELETE_FAIL,
+  BOARD_OF_DIRECTOR_DELETE_REQUEST,
+  BOARD_OF_DIRECTOR_DELETE_SUCCESS,
+  BOARD_OF_DIRECTOR_DETAILS_FAIL,
+  BOARD_OF_DIRECTOR_DETAILS_REQUEST,
+  BOARD_OF_DIRECTOR_DETAILS_SUCCESS,
+  BOARD_OF_DIRECTOR_LIST_FAIL,
+  BOARD_OF_DIRECTOR_LIST_REQUEST,
+  BOARD_OF_DIRECTOR_LIST_SUCCESS,
+  BOARD_OF_DIRECTOR_UPDATE_FAIL,
+  BOARD_OF_DIRECTOR_UPDATE_REQUEST,
+  BOARD_OF_DIRECTOR_UPDATE_SUCCESS
+} from './../constants/BoardOfDirectorConstants'
+import { logout } from './userActions'
+
+export const listBoardOfDirector = () => async (dispatch) => {
+  try {
+    dispatch({ type: BOARD_OF_DIRECTOR_LIST_REQUEST })
+    const { data } = await axios.get(`/api/boardOfDirectors`)
+    dispatch({ type: BOARD_OF_DIRECTOR_LIST_SUCCESS, payload: data })
+  } catch (error) {
+    dispatch({ type: BOARD_OF_DIRECTOR_LIST_FAIL, payload: error.response && error.response.data.message ? error.response.data.message : error.message })
+  }
+}
+
+export const listBoardOfDirectorDetails = (id) => async (dispatch) => {
+  try {
+    dispatch({ type: BOARD_OF_DIRECTOR_DETAILS_REQUEST })
+    const { data } = await axios.get(`/api/boardOfDirectors/${id}`)
+    dispatch({ type: BOARD_OF_DIRECTOR_DETAILS_SUCCESS, payload: data })
+  } catch (error) {
+    dispatch({ type: BOARD_OF_DIRECTOR_DETAILS_FAIL, payload: error.response && error.response.data.message ? error.response.data.message : error.message })
+  }
+}
+
+export const deleteBoardOfDirector = (id) => async (dispatch, getState) => {
+  try {
+    dispatch({ type: BOARD_OF_DIRECTOR_DELETE_REQUEST })
+    const {
+      userLogin: { userInfo },
+    } = getState()
+
+    const config = { headers: { Authorization: `Bearer ${userInfo.token}` } }
+    await axios.delete(`/api/boardOfDirectors/${id}`, config)
+    dispatch({ type: BOARD_OF_DIRECTOR_DELETE_SUCCESS })
+  } catch (error) {
+    const message = error.response && error.response.data.message ? error.response.data.message : error.message
+    if (message === 'Not authorized, token failed') {
+      dispatch(logout())
+    }
+    dispatch({ type: BOARD_OF_DIRECTOR_DELETE_FAIL, payload: message })
+  }
+}
+
+export const createBoardOfDirector = () => async (dispatch, getState) => {
+  try {
+    dispatch({ type: BOARD_OF_DIRECTOR_CREATE_REQUEST })
+
+    const {
+      userLogin: { userInfo },
+    } = getState()
+
+    const config = { headers: { Authorization: `Bearer ${userInfo.token}` } }
+    const { data } = await axios.post(`/api/boardOfDirectors`, {}, config)
+    dispatch({ type: BOARD_OF_DIRECTOR_CREATE_SUCCESS, payload: data })
+  } catch (error) {
+    const message = error.response && error.response.data.message ? error.response.data.message : error.message
+    if (message === 'Not authorized, token failed') {
+      dispatch(logout())
+    }
+    dispatch({ type: BOARD_OF_DIRECTOR_CREATE_FAIL, payload: message })
+  }
+}
+
+export const updateBoardOfDirector = (boardOfDirector) => async (dispatch, getState) => {
+  try {
+    dispatch({ type: BOARD_OF_DIRECTOR_UPDATE_REQUEST })
+
+    const {
+      userLogin: { userInfo },
+    } = getState()
+
+    const config = { headers: { 'Content-Type': 'application/json', Authorization: `Bearer ${userInfo.token}` } }
+    const { data } = await axios.put(`/api/boardOfDirectors/${boardOfDirector._id}`, boardOfDirector, config)
+
+    dispatch({ type: BOARD_OF_DIRECTOR_UPDATE_SUCCESS, payload: data })
+    dispatch({ type: BOARD_OF_DIRECTOR_DETAILS_SUCCESS, payload: data })
+  } catch (error) {
+    const message = error.response && error.response.data.message ? error.response.data.message : error.message
+    if (message === 'Not Authorized, Token Failed') {
+      dispatch(logout())
+    }
+    dispatch({ type: BOARD_OF_DIRECTOR_UPDATE_FAIL, payload: message })
+  }
+}
---
+++ frontend/src/redux/reducers/BoardOfDirectorReducers.js
@@ -0,0 +1,88 @@
+import {
+  BOARD_OF_DIRECTOR_CREATE_FAIL,
+  BOARD_OF_DIRECTOR_CREATE_REQUEST,
+  BOARD_OF_DIRECTOR_CREATE_RESET,
+  BOARD_OF_DIRECTOR_CREATE_SUCCESS,
+  BOARD_OF_DIRECTOR_DELETE_FAIL,
+  BOARD_OF_DIRECTOR_DELETE_REQUEST,
+  BOARD_OF_DIRECTOR_DELETE_SUCCESS,
+  BOARD_OF_DIRECTOR_DETAILS_FAIL,
+  BOARD_OF_DIRECTOR_DETAILS_REQUEST,
+  BOARD_OF_DIRECTOR_DETAILS_SUCCESS,
+  BOARD_OF_DIRECTOR_LIST_FAIL,
+  BOARD_OF_DIRECTOR_LIST_REQUEST,
+  BOARD_OF_DIRECTOR_LIST_SUCCESS,
+  BOARD_OF_DIRECTOR_UPDATE_FAIL,
+  BOARD_OF_DIRECTOR_UPDATE_REQUEST,
+  BOARD_OF_DIRECTOR_UPDATE_RESET,
+  BOARD_OF_DIRECTOR_UPDATE_SUCCESS
+} from './../constants/BoardOfDirectorConstants'
+
+export const boardOfDirectorListReducer = (state = { boardOfDirectors: [] }, action) => {
+  switch (action.type) {
+    case BOARD_OF_DIRECTOR_LIST_REQUEST:
+      return { loading: true, boardOfDirectors: [] }
+    case BOARD_OF_DIRECTOR_LIST_SUCCESS:
+      return { loading: false, boardOfDirectors: action.payload }
+    case BOARD_OF_DIRECTOR_LIST_FAIL:
+      return { loading: false, error: action.payload }
+    default:
+      return state
+  }
+}
+
+export const boardOfDirectorDetailsReducer = (state = { boardOfDirector: { reviews: [] } }, action) => {
+  switch (action.type) {
+    case BOARD_OF_DIRECTOR_DETAILS_REQUEST:
+      return { ...state, loading: true }
+    case BOARD_OF_DIRECTOR_DETAILS_SUCCESS:
+      return { loading: false, boardOfDirector: action.payload }
+    case BOARD_OF_DIRECTOR_DETAILS_FAIL:
+      return { loading: false, error: action.payload }
+    default:
+      return state
+  }
+}
+
+export const boardOfDirectorDeleteReducer = (state = {}, action) => {
+  switch (action.type) {
+    case BOARD_OF_DIRECTOR_DELETE_REQUEST:
+      return { loading: true }
+    case BOARD_OF_DIRECTOR_DELETE_SUCCESS:
+      return { loading: false, success: true }
+    case BOARD_OF_DIRECTOR_DELETE_FAIL:
+      return { loading: false, error: action.payload }
+    default:
+      return state
+  }
+}
+
+export const boardOfDirectorCreateReducer = (state = {}, action) => {
+  switch (action.type) {
+    case BOARD_OF_DIRECTOR_CREATE_REQUEST:
+      return { loading: true }
+    case BOARD_OF_DIRECTOR_CREATE_SUCCESS:
+      return { loading: false, success: true, boardOfDirector: action.payload }
+    case BOARD_OF_DIRECTOR_CREATE_FAIL:
+      return { loading: false, error: action.payload }
+    case BOARD_OF_DIRECTOR_CREATE_RESET:
+      return {}
+    default:
+      return state
+  }
+}
+
+export const boardOfDirectorUpdateReducer = (state = { boardOfDirector: {} }, action) => {
+  switch (action.type) {
+    case BOARD_OF_DIRECTOR_UPDATE_REQUEST:
+      return { loading: true }
+    case BOARD_OF_DIRECTOR_UPDATE_SUCCESS:
+      return { loading: false, success: true, boardOfDirector: action.payload }
+    case BOARD_OF_DIRECTOR_UPDATE_FAIL:
+      return { loading: false, error: action.payload }
+    case BOARD_OF_DIRECTOR_UPDATE_RESET:
+      return { boardOfDirector: {} }
+    default:
+      return state
+  }
+}
