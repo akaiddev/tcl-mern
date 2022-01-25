fed9c39bee0fff2de6c9712b5d5abc9c21930a0b
abdurrahmanakaid <akaid.dev@gmail.com>
2022-01-24 16:10:16 -0800

    add some udpdate

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,11 +3,7 @@ import mongoose from 'mongoose'
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-    })
-
+    const conn = await mongoose.connect(process.env.MONGO_URI, {useUnifiedTopology: true, useNewUrlParser: true})
     console.log(colors.cyan.bold.underline(`TCL Database Connected: ${conn.connection.host}`))
   } catch (error) {
     console.error(`Error: ${error.message}`.red.underline.bold)
