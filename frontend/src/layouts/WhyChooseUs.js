987f07cca555cc9a0cd7ac098db659633141b3b8
abdurrahmanakaid <akaid.dev@gmail.com>
2022-01-25 13:02:20 -0800

    addd uopdate

--- frontend/src/layouts/WhyChooseUs.js
+++ frontend/src/layouts/WhyChooseUs.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Col, Container, Row } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import Loader from './../common/Loader'
+import Message from './../common/Message'
+import { listCorporate } from './../redux/actions/CorporateActions'
 
 const WhyChooseUs = () => {
+  const dispatch = useDispatch()
+
+  const corporateList = useSelector((state) => state.corporateList)
+
+  const { loading, error, corporates } = corporateList
+
+  useEffect(() => {
+    dispatch(listCorporate())
+  }, [dispatch])
+
   return (
     <div className='feat bg-gray pt-5 pb-5'>
       <Container>
@@ -10,68 +24,26 @@ const WhyChooseUs = () => {
             <h4>
               <span>Why Choose</span> Us?
             </h4>
-            <p>
-              When you choose us, you'll feel the benefit of 10 years' experience of Web Development. Because we know the digital world and we know that how to handle it.
-              With working knowledge of online, SEO and social media.
-            </p>
           </Col>
-          <Col sm={6} lg={4}>
-            <div className='item'>
-              <span className='icon feature_box_col_one'>
-                <i className='fa fa-globe' />
-              </span>
-              <h6>Modern Design</h6>
-              <p>We use latest technology for the latest world because we know the demand of peoples.</p>
-            </div>
-          </Col>
-          <Col sm={6} lg={4}>
-            <div className='item'>
-              <span className='icon feature_box_col_two'>
-                <i className='fa fa-anchor' />
-              </span>
-              <h6>Creative Design</h6>
-              <p>We are always creative and and always lisen our costomers and we mix these two things and make beast design.</p>
-            </div>
-          </Col>
-          <Col sm={6} lg={4}>
-            <div className='item'>
-              <span className='icon feature_box_col_three'>
-                <i className='fa fa-hourglass-half' />
-              </span>
-              <h6>24 x 7 User Support</h6>
-              <p>If our customer has any problem and any query we are always happy to help then.</p>
-            </div>
-          </Col>
-          <Col sm={6} lg={4}>
-            <div className='item'>
-              <span className='icon feature_box_col_four'>
-                <i className='fa fa-database' />
-              </span>
-              <h6>Business Growth</h6>
-              <p>Everyone wants to live on top of the mountain, but all the happiness and growth occurs while you're climbing it</p>
-            </div>
-          </Col>
-          <Col sm={6} lg={4}>
-            <div className='item'>
-              <span className='icon feature_box_col_five'>
-                <i className='fa fa-upload' />
-              </span>
-              <h6>Market Strategy</h6>
-              <p>
-                Holding back technology to preserve broken business models is like allowing blacksmiths to veto the internal combustion engine in order to protect their
-                horseshoes.
-              </p>
-            </div>
-          </Col>
-          <Col sm={6} lg={4}>
-            <div className='item'>
-              <span className='icon feature_box_col_six'>
-                <i className='fa fa-camera' />
-              </span>
-              <h6>Affordable cost</h6>
-              <p>Love is a special word, and I use it only when I mean it. You say the word too much and it becomes cheap.</p>
-            </div>
-          </Col>
+          {loading ? (
+            <Loader />
+          ) : error ? (
+            <Message variant='danger'>{error}</Message>
+          ) : (
+            <>
+              {corporates.map((corporate) => (
+                <Col sm={6} lg={4}>
+                  <div className='item'>
+                    <span className='icon feature_box_col_one'>
+                      <i className='fa fa-globe' />
+                    </span>
+                    <h6>{corporate.name}</h6>
+                    <p>{corporate.description.map((items) => items)}</p>
+                  </div>
+                </Col>
+              ))}
+            </>
+          )}
         </Row>
       </Container>
     </div>
